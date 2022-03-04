13e1b8edaf81659423b026985d255af2b5afa708
ZyrexDjewelG <99930776+ZyrexDjewelG@users.noreply.github.com>
2022-03-04 09:26:37 +0800

    Add files via upload

---
+++ CMSC21/Lecture1/assignment_lec1_c1.c
@@ -0,0 +1,13 @@
+//Lecture 1 Assignments N0. 1//
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\na. In C, lowercase letters are significant.\n");
+	printf("b. main is where program execution begins.\n");
+	printf("c. Opening and closing braces enclose program statements in a routine.\n");
+	printf("d. All program statements must be terminate by a semicolon.\n");
+	
+	return 0;
+}

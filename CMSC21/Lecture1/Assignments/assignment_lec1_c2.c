0f4ccf349be3e0c7f84573fd9501f90d65adfd88
ZyrexDjewelG <99930776+ZyrexDjewelG@users.noreply.github.com>
2022-03-04 09:29:10 +0800

    Add files via upload

---
+++ CMSC21/Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,17 @@
+//Lecture 1 Assignments N0. 3//
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2, ans;
+
+	num1 = 87;
+	num2 = 15;
+	ans = num1 - num2;
+
+	printf("%d minus %d is equal to %d", num1, num2, ans);
+
+	return 0;
+
+}

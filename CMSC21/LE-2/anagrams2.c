371718f09576aa06115bca3c3959589e587b1846
ZyrexDjewelG <99930776+ZyrexDjewelG@users.noreply.github.com>
2022-05-23 22:59:05 +0800

    Add files via upload

---
+++ CMSC21/LE-2/anagrams2.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <ctype.h> /* toupper, isalpha */
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+void scan_word(int occurences[26]);
+bool is_anagram(int occurences1[26], int occurences2[26]);
+
+int i, *ptr1, *ptr2;
+
+int main(void) {
+
+    int occurences1[26] = {0}, occurences2[26] = {0};
+
+    printf("\nEnter first word: ");
+    scan_word(occurences1);
+
+    printf("\nEnter second word: ");
+    scan_word(occurences2);
+
+    if(is_anagram(occurences1, occurences2)){
+        printf("\nThe words are anagrams");
+        return 0;
+    }
+    else{
+        printf("\nThe words are not anagram.");
+        return 0;
+    }
+}
+
+void scan_word(int occurences[26]){
+    char c;
+
+    while((c = getchar()) != '\n'){
+        if(isalpha(c)){
+            occurences[toupper(c) - 'A']++;
+        }
+    }
+}
+
+bool is_anagram(int occurences1[26], int occurences2[26]){
+
+    for(i = 0; i < 26; i++){
+        if(occurences1[i] != occurences2[i]){
+            return false;
+        }
+        else
+            return true;
+    }
+}
+
+#include <stdio.h>
+#include <ctype.h> /* toupper, isalpha */
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+void scan_word(int occurences[26]);
+bool is_anagram(int occurences1[26], int occurences2[26]);
+
+int i;
+
+int main(void) {
+
+    int occurences1[26] = {0}, occurences2[26] = {0};
+
+    printf("\nEnter first word: ");
+    scan_word(occurences1);
+
+    printf("\nEnter second word: ");
+    scan_word(occurences2);
+
+    if(is_anagram(occurences1, occurences2)){
+        printf("\nThe words are anagrams");
+        return 0;
+    }
+    else{
+        printf("\nThe words are not anagram.");
+        return 0;
+    }
+}
+
+void scan_word(int occurences[26]){
+    char c;
+
+    while((c = getchar()) != '\n'){
+        if(isalpha(c)){
+            occurences[toupper(c) - 'A']++;
+        }
+    }
+}
+
+bool is_anagram(int occurences1[26], int occurences2[26]){
+    ptr1 = occurences1;
+    ptr2 = occurences2;
+    for(i = 0; i < 26; i++){
+        if(*(ptr1+i) != *(ptr2+1)){
+            return false;
+        }
+        else
+            return true;
+    }
+}
+
